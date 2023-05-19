f5bd280563085fc0c60befcda86f8719558213e4
upredic <unpredic326@qq.com>
2023-05-19 09:32:25 +0800

    添加公共字段自动填充功能

--- src/main/java/com/example/reggie_alpha/controller/EmployeeController.java
+++ src/main/java/com/example/reggie_alpha/controller/EmployeeController.java
@@ -46,11 +46,11 @@ public class EmployeeController {
     public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
         log.info("新增员工，员工信息：{}", employee.toString());
         employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
-        Long empId = (Long) request.getSession().getAttribute("employee");
-        employee.setCreateUser(empId);
-        employee.setUpdateUser(empId);
+//        employee.setCreateTime(LocalDateTime.now());
+//        employee.setUpdateTime(LocalDateTime.now());
+//        Long empId = (Long) request.getSession().getAttribute("employee");
+//        employee.setCreateUser(empId);
+//        employee.setUpdateUser(empId);
         employeeService.save(employee);
         return R.success("新增员工成功");
     }
@@ -73,8 +73,8 @@ public class EmployeeController {
     public R<String> update(HttpServletRequest request, @RequestBody Employee employee) {
         log.info(employee.toString());
         Long empId = (Long) request.getSession().getAttribute("employee");
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(empId);
+//        employee.setUpdateTime(LocalDateTime.now());
+//        employee.setUpdateUser(empId);
         employeeService.updateById(employee);
         return R.success("员工信息修改成功");
     }
--- src/main/java/com/example/reggie_alpha/filter/LoginCheckFilter.java
+++ src/main/java/com/example/reggie_alpha/filter/LoginCheckFilter.java
@@ -1,6 +1,7 @@
 package com.example.reggie_alpha.filter;
 
 import com.alibaba.fastjson.JSON;
+import com.example.reggie_alpha.common.BaseContext;
 import com.example.reggie_alpha.common.R;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
@@ -44,6 +45,10 @@ public class LoginCheckFilter implements Filter {
         // 如果登录，直接放行
         if (request.getSession().getAttribute("employee") != null) {
             log.info("用户已登陆：{}", request.getSession().getAttribute("employee"));
+
+            Long empId = (Long) request.getSession().getAttribute("employee");
+            BaseContext.setCurrentId(empId);
+
             filterChain.doFilter(request, response);
             return;
         }
